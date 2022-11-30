760b2e6aceae9cbede61b0c6ef5e6563fe8866b0
Juan Franco <jfrancoh5@gmail.com>
2022-11-30 18:26:46 -0500

    estilos de las card animadas terminado

--- src/App.js
+++ src/App.js
@@ -9,25 +9,6 @@ import Footer from './containers/Footer/Footer';
 
 
 function App() {
-  const getRandomNumberRecipes = (min,max) =>{
-    return parseInt(Math.random() * (max - min) + min);
-  }
-
-  let randomNumber = getRandomNumberRecipes(52770,52780);
-
-  const API = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${randomNumber}`;
-
-  const [recipes,setRecipes]= React.useState([]);
-
-  useEffect(()=>{
-    async function fetchData(){
-      const response = await axios(API);
-      setRecipes(response.data);
-    }
-    fetchData();
-  },[])
-
-  console.log(recipes);
   return (
     <div className="App">
       <Header/>
--- src/components/NewRecipeCard/NewRecipeCard.jsx
+++ src/components/NewRecipeCard/NewRecipeCard.jsx
@@ -3,11 +3,13 @@ import './NewRecipeCard.scss';
 import recipeImage from '../../assets/Ojingeo-muchim.png';
 import star from '../../assets/icons/ic_star.svg';
 import hearth from '../../assets/icons/ic-favorite.svg';
+import plate from '../../assets/icons/ic_portion.svg';
+import clock from '../../assets/icons/ic_time.svg';
+import chefIcon from '../../assets/icons/ic_chef.svg';
 
-const NewRecipeCard = () => {
+const FirstRecipe = () =>{
   return(
-    <article className="new-recipe-card">
-       <img src={recipeImage} className="recipe-image"/>
+    <section>
        <p className='new-recipe-name'>Ojingeo <span>Muchim</span></p>
        <div className='new-recipe-qualification'>
           <div className='new-recipe-first-group-ranking'>
@@ -15,7 +17,55 @@ const NewRecipeCard = () => {
             <p>5.0</p>
           </div>
           <img className="new-recipe-hearth" src={hearth}/>
-       </div> 
+       </div>
+       </section> 
+  )
+}
+
+const InformationRecipe = () =>{
+  return (
+    <section className='information-recipe'>
+      <article className='portion-size'>
+        <img className="plate-image" src={plate}/>
+        <p>Tamaño de la porción</p>
+        <p>4 raciones</p>  
+      </article>
+      <article className='time-preparation'>
+        <img className='clock-image' src={clock}/>
+        <p>Tiempo de preparación</p>
+        <p>10 minutos</p>
+      </article>
+      <article className='recipe-difficult'>
+        <img className='chef-icon' src={chefIcon}/>
+        <p>Dificultad</p>
+        <p>Facil</p>
+      </article>
+      
+    </section>
+  )
+}
+
+const NewRecipeCard = () => {
+  const [sizeCard, setSizeCard] = React.useState('new-recipe-card');
+  const screenWidth = window.screen.width;
+  console.log(screenWidth);
+
+  const changePerformanceCard = () =>{
+    if(screenWidth > 1278){
+      setSizeCard('new-recipe-card new-recipe-card-enlarged')
+    }
+  }
+
+  const changeDefaultCard = ()=>{
+    if(screenWidth > 1278){
+      setSizeCard('new-recipe-card');
+    }
+  }
+
+  return(
+    <article className={sizeCard}  onMouseOver={changePerformanceCard} onMouseLeave={changeDefaultCard}>
+       <img src={recipeImage} className="recipe-image"/>
+      {(sizeCard==="new-recipe-card")?<FirstRecipe/>:<InformationRecipe/>} 
     </article>
   )
 }
